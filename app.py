8ff981a3d74d481d0ef7daee2a9c95d3ff1edb42
Haseera-Saib <162562671+Haseera-Saib@users.noreply.github.com>
2024-09-27 17:14:19 +0000

    3

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ import plotly.express as px
 
 st.set_page_config(layout='wide')
 
-st.header('COVID DETAILS ')
+st.markdown("<h2 style='text-align: center;'>COVID DETAILS</h2>", unsafe_allow_html=True)
 data=pd.read_csv('/workspaces/covid_dashboard/Covid Dashboard_data1.csv')
-with st.expander('show more'):
-    st.write(data)
+#with st.expander('show more'):
+    #st.write(data)
 
-st.subheader('Covid Details Of India')
+st.markdown("<h3 style='text-align: center;'>Covid detailes of India</h3>", unsafe_allow_html=True)
 ct=st.container(border=True)
-c1,c2,c3,c4=ct.columns(4)
+c1,c2,=ct.columns(2)
+c3,c4 =ct.columns(2)
 c1.metric('Total Covid Cases',data['Total Cases'].sum())
 c2.metric('Recovered',data['Discharged'].sum())
 c3.metric('Death',data['Deaths'].sum())
@@ -21,81 +22,108 @@ d=data['State/UTs'].unique()
 s=st.selectbox('Select State',d)
 df=data[data['State/UTs']==s]
 
+st.markdown("<h3 style='text-align: center;'>Covid Details of States</h3>", unsafe_allow_html=True)
 
 st.subheader('Covid Details of States')
 
 ctn=st.container(border=True)
-cl1,cl2,cl3,cl4=ctn.columns(4)
+cl1,cl2=ctn.columns(2)
+cl3,cl4=ctn.columns(2)
+
 
 cl1.metric('Total Covid Cases',df['Total Cases'].sum())
 cl2.metric('Recovered',df['Discharged'].sum())
 cl3.metric('Death',df['Deaths'].sum())
 cl4.metric('Active',df['Active'].sum())
 
+ctnt=st.container(border=True)
+ctnt.markdown("<h3 style='text-align: center;'>Total Cases</h3>", unsafe_allow_html=True)
+col1,col2=ctnt.columns([1,1],)
 
-
-st.subheader("Total Cases")
-
-st.subheader('highest')
+col1.markdown("<h4 style='text-align: center;'>Highest</h4>", unsafe_allow_html=True)
 high = data.nlargest(5, 'Total Cases')[['State/UTs', 'Total Cases']]
 high.rename(columns={'State/UTs': 'States', 'Total Cases': 'Cases'}, inplace=True)
-st.dataframe(high)
+col1.dataframe(high,use_container_width=True)
 
-
-st.header('Least')
+col2.markdown("<h4 style='text-align: center;'>Least</h4>", unsafe_allow_html=True)
 least = data[data['Total Cases'] > 0].nsmallest(5, 'Total Cases')[['State/UTs', 'Total Cases']]
 least.rename(columns={'State/UTs': 'States', 'Total Cases': 'Cases'}, inplace=True)
-st.dataframe(least)
+col2.dataframe(least,use_container_width=True)
 
 
-st.subheader("Recovered")
+cont=st.container(border=True)
+cont.markdown("<h3 style='text-align: center;'>Recovered</h3>", unsafe_allow_html=True)
+cool1, cool2 = cont.columns([1, 1])
 
-st.subheader('highest')
+cool1.markdown("<h4 style='text-align: center;'>Highest</h4>", unsafe_allow_html=True)
 high1 = data.nlargest(5, 'Discharge Ratio')[['State/UTs', 'Discharge Ratio']]
 high1.rename(columns={'State/UTs': 'States', 'Discharge Ratio': 'Recovered'}, inplace=True)
-st.dataframe(high1)
+cool1.dataframe(high1, use_container_width=True)
 
-st.header('Least')
+cool2.markdown("<h4 style='text-align: center;'>Least</h4>", unsafe_allow_html=True)
 least1 = data[data['Discharge Ratio'] > 0].nsmallest(5, 'Discharge Ratio')[['State/UTs', 'Discharge Ratio']]
 least1.rename(columns={'State/UTs': 'States', 'Discharge Ratio': 'Recovered'}, inplace=True)
-st.dataframe(least1)
+cool2.dataframe(least1, use_container_width=True)
 
 
-st.subheader("Deaths")
+contain=st.container(border=True)
+contain.markdown("<h4 style='text-align: center;'>Deaths</h4>", unsafe_allow_html=True)
+cll1,cll2=contain.columns([1,1])
 
-st.subheader('highest')
+cll1.markdown("<h4 style='text-align: center;'>Highest</h4>", unsafe_allow_html=True)
 high2 = data.nlargest(5, 'Death Ratio')[['State/UTs', 'Death Ratio']]
 high2.rename(columns={'State/UTs': 'States', 'Death Ratio': 'Deaths'}, inplace=True)
-st.dataframe(high2)
+cll1.dataframe(high2,use_container_width=True)
 
-st.header('Least')
+cll2.markdown("<h4 style='text-align: center;'>Least</h4>", unsafe_allow_html=True)
 least2 = data[data['Death Ratio'] > 0].nsmallest(5, 'Death Ratio')[['State/UTs', 'Death Ratio']]
 least2.rename(columns={'State/UTs': 'States', 'Death Ratio': 'Deaths'}, inplace=True)
-st.dataframe(least2)
+cll2.dataframe(least2,use_container_width=True)
 
 
 
+container=st.container(border=True)
+container.markdown("<h3 style='text-align: center;'>Active</h3>", unsafe_allow_html=True)
+coo1,coo2 = container.columns([1,1])
 
-st.subheader("Actives")
-
-st.subheader('highest')
+coo1.markdown("<h4 style='text-align: center;'>Highest</h4>", unsafe_allow_html=True)
 high3 = data.nlargest(5, 'Active Ratio')[['State/UTs', 'Active Ratio']]
 high3.rename(columns={'State/UTs': 'States', 'Active Ratio': 'Active'}, inplace=True)
-st.dataframe(high3)
+coo1.dataframe(high3,use_container_width=True)
 
-st.header('Least')
+coo2.markdown("<h4 style='text-align: center;'>Least</h4>", unsafe_allow_html=True)
 least3 = data[data['Active Ratio'] > 0].nsmallest(5, 'Active Ratio')[['State/UTs', 'Active Ratio']]
 least3.rename(columns={'State/UTs': 'States', 'Active Ratio': 'Active'}, inplace=True)
-st.dataframe(least3)
+coo2.dataframe(least3,use_container_width=True)
 
 
-st.metric('Death Average(Death Value)',df['Death Ratio'].mean())
-top= data.nlargest(15, 'Death Ratio')
-bar = px.bar(top, y='State/UTs', x='Death Ratio', title='Death Ratio by State (%)')
-st.write(bar)
+
+colum1,colum2=st.columns(2)
+colum3,colum4=st.columns(2)
 
 
-st.metric('Recovery Average(recover Value)',df['Discharged'].mean())
-top1= data.nlargest(10, 'Discharged')
+colum1.metric('Death Average(Death Value)',df['Death Ratio'].mean())
+#top= data.nlargest(15, 'Death Ratio')
+bar = px.bar(data, y='State/UTs', x='Death Ratio', title='Death Ratio by State (%)',color_continuous_scale='Viridis')
+colum1.plotly_chart(bar)
+
+
+colum2.metric('Recovery Average(recover Value)',df['Discharged'].mean())
+#top1= data.nlargest(10, 'Discharged')
 bar1 = px.bar(data, y='State/UTs', x='Discharged', title='Recovery Ratio by State (%)')
-st.write(bar1)
+colum2.write(bar1)
+
+
+colum3.metric('Active Average(active Value)',df['Active'].mean())
+fig3 = px.bar(data, 
+               x='State/UTs', 
+               y=['Active', 'Deaths'], 
+               title='Active Cases and Deaths',
+               labels={'value': 'Count', 'variable': 'Category'},
+               barmode='group')
+colum3.write(fig3)
+
+colum4.metric('Population Average',df['Population'].mean())
+fig4=px.bar(data,x='State/UTs',y=['Total Cases','Discharged'],barmode='group',
+color_discrete_sequence=['#28a745', '#dc3545'])
+colum4.plotly_chart(fig4)
